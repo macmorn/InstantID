5438a3700212af9745ec5b55f1102a8bc171d6c9
macmorn <anton1995@t-online.de>
2024-07-07 12:38:30 +0000

    test

--- tests/test_instant_id.py
+++ tests/test_instant_id.py
@@ -1,7 +1,13 @@
+import os
+import sys
+
+sys.path.append("./")
 from gird_search_images import InstantIDModelPipe
 
 
 def test_single_image_generation():
+    # assert that te image exists
+    assert os.path.exists("examples/musk_resize.jpeg")
     model = InstantIDModelPipe(xformers=True)
     model._initalize_pipeline(pretrained_model_name_or_path="wangqixun/YamerMIX_v8")
     image = model._generate_single_image(
@@ -9,16 +15,19 @@ def test_single_image_generation():
         pose_image_path=None,
         prompt="A portrait of a person in a clown costume",
         negative_prompt="",
-        num_steps=30,
+        num_steps=15,
+        guidance_scale=5,
         identitynet_strength_ratio=0.5,
         adapter_strength_ratio=0.5,
-        controlnet_strength_ratio=0.5,
-        pose_strength=0.5,
+        pose_strength=0,
         canny_strength=0,
         depth_strength=0,
         controlnet_selection=[],
-        guidance_scale=5,
     )
     assert image is not None
     # save the image
     image.save("examples/musk_clown.jpg")
+
+
+if __name__ == "__main__":
+    test_single_image_generation()
